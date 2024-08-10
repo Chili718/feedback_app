78879dfb57d40861af95befcd2cb62abb546f69a
Jon Tice <chilifreez@aol.com>
2024-08-10 17:13:14 -0400

    Created Feedback UI and Deletion Functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
+import { useState } from 'react';
 import Header from './components/Header';
+import FeedbackList from './components/FeedbackList';
+import FeedbackData from './data/FeedbackData';
+import FeedbackStats from './components/FeedbackStats';
 
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
+
+  const deleteFeedback = (id) => {
+    if(window.confirm('Are you sure you want to delete?')){
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+    
+  }
 
   return (
     <>
-      <Header />
+      <Header  />
+      <FeedbackStats feedback={feedback}/>
       <div className='container'>
-        <h1>My App</h1>
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
       </div>
     </>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,14 @@
 import PropTypes from 'prop-types'; /* import prop types impt */
 
-function Header({text = 'Feedback UI'}) {
+function Header({text = 'Feedback UI', bgColor = 'rgba(0,0,0,0.4)', textColor = '#ff6a95'}) {
+
+    const headerStyles = {
+        backgroundColor:  bgColor,
+        color: textColor,
+    }
+
   return (
-    <header>
+    <header style={headerStyles}>
       <div className='container'>
         <h2>{text}</h2>
       </div>
@@ -10,15 +16,21 @@ function Header({text = 'Feedback UI'}) {
   )
 }
 
-/*
-Alternate Way to set the default prop types
 
-Header.defaultProps = {
+/* Alternate Way to set the default prop types 
+  This will be phased out soon
+*/
+
+/* Header.defaultProps = {
     text: 'Feedback UI',
+    bgColor: 'rgba(0,0,0,0.4)',
+    textColor: '#ff6a95',
 } */
 
-/* Header.propTypes = {
+Header.propTypes = {
     text: PropTypes.string,
-} */
+    bgColor: PropTypes.string,
+    textColor: PropTypes.string,
+}
 
 export default Header;
